3981e3b68d1160f236254cb1a9f767e6f203bde3
OleksandraNochovna <nochovna.sasha@gmail.com>
2024-11-14 18:38:44 +0100

    integrate task service to task actions

--- src/actions/task/task.ts
+++ src/actions/task/task.ts
@@ -1,7 +1,5 @@
 'use server';
 
-import prisma from '@/prisma-client';
-import { TaskRole } from '@prisma/client';
 import {
   AddTaskParams,
   UpdateTaskParams,
@@ -9,87 +7,45 @@ import {
   ToggleTaskParams,
 } from './types';
 import { requireAuth } from '../auth/middlewares';
+import { taskService } from './service';
 
 export const getTasks = await requireAuth(({ session }) => {
-  const authorId = session.user.id;
-
-  return prisma.task.findMany({
-    where: { assignments: { some: { userId: authorId } } },
-  });
+  return taskService.getUserTasks(session.user.id);
 });
 
 export const getTaskById = await requireAuth(({ session }, id: string) => {
-  const authorId = session.user.id;
-
-  return prisma.task.findUnique({
-    where: { id, assignments: { some: { userId: authorId } } },
-  });
+  return taskService.getUserTaskById(session.user.id, id);
 });
 
 export const addTask = await requireAuth(
   ({ session }, { title, description }: AddTaskParams) => {
-    const authorId = session.user.id;
-
-    return prisma.task.create({
-      data: {
-        title,
-        description: description ?? null,
-        assignments: {
-          create: { userId: authorId, role: TaskRole.OWNER, accepted: true },
-        },
-      },
-    });
+    return taskService.createTask(session.user.id, { title, description });
   },
 );
 
 export const updateTask = await requireAuth(
   async ({ session }, { id, title, description }: UpdateTaskParams) => {
     const authorId = session.user.id;
-    const task = await getTaskById(id);
+    const task = await taskService.getUserTaskByIdOrThrow(authorId, id);
 
-    if (!task) {
-      throw new Error('Task not found');
-    }
-
-    return prisma.task.update({
-      where: { id, assignments: { some: { userId: authorId } } },
-      data: {
-        title,
-        ...(description !== undefined && { description }),
-      },
-    });
+    return taskService.updateTaskById(task.id, { title, description });
   },
 );
 
 export const deleteTask = await requireAuth(
   async ({ session }, { id }: DeleteTaskParams) => {
     const authorId = session.user.id;
-    const task = await getTaskById(id);
+    const task = await taskService.getUserTaskByIdOrThrow(authorId, id);
 
-    if (!task) {
-      throw new Error('Task not found');
-    }
-
-    return prisma.task.delete({
-      where: { id, assignments: { some: { userId: authorId } } },
-    });
+    return taskService.deleteTaskById(task.id);
   },
 );
 
 export const toggleTaskStatus = await requireAuth(
   async ({ session }, { id }: ToggleTaskParams) => {
     const authorId = session.user.id;
-    const task = await getTaskById(id);
+    const task = await taskService.getUserTaskByIdOrThrow(authorId, id);
 
-    if (!task) {
-      throw new Error('Task not found');
-    }
-
-    return prisma.task.update({
-      where: { id, assignments: { some: { userId: authorId } } },
-      data: {
-        completed: !task.completed,
-      },
-    });
+    return taskService.updateTaskById(task.id, { completed: !task.completed });
   },
 );
