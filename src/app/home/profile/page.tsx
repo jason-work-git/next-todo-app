a834ccc2a8064ac7640494f7a41db3006da8b801
Daniel <jamoja64749@gmail.com>
2024-11-14 19:56:22 +0100

    modified settings page layout

--- src/app/home/profile/page.tsx
+++ src/app/home/profile/page.tsx
@@ -1,47 +1,35 @@
 import { Button } from '@/components/ui/button';
 import { Label } from '@/components/ui/label';
 import { Input } from '@/components/ui/input';
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { Tabs } from '@/components/ui/tabs';
 
 export default function Page() {
   return (
-    <div className="space-y-6">
-      <div>
+    <div className="space-y-6 p-4 sm:p-8">
+      <div className="text-center">
         <h2 className="text-2xl font-bold tracking-tight">Settings</h2>
         <p className="text-muted-foreground">
           Manage your account settings and set e-mail preferences.
         </p>
       </div>
-      <Tabs defaultValue="account" className="w-full">
-        <TabsList>
-          <TabsTrigger value="account">Account</TabsTrigger>
-          <TabsTrigger value="password">Password</TabsTrigger>
-        </TabsList>
-        <TabsContent value="account" className="space-y-4">
-          {/* Profile Name */}
-          <div className="space-y-2 max-w-[430]">
-            <Label htmlFor="name">Name</Label>
-            <Input id="name" defaultValue="John Doe" />
-          </div>
-          {/* Email */}
-          <div className="space-y-2 max-w-[430]">
-            <Label htmlFor="email">Email</Label>
-            <Input id="email" defaultValue="name@example.com" />
-          </div>
-          <Button>Save changes</Button>
-        </TabsContent>
-        {/* Change Password */}
-        <TabsContent value="password" className="space-y-4">
-          <div className="space-y-2 max-w-[436]">
-            <Label htmlFor="current">Current password</Label>
-            <Input id="current" type="password" />
-          </div>
-          <div className="space-y-2 max-w-[436]">
-            <Label htmlFor="new">New password</Label>
-            <Input id="new" type="password" />
-          </div>
-          <Button disabled>Change password</Button>
-        </TabsContent>
+      <Tabs className="space-y-4">
+        {/* Profile Name */}
+        <div className="space-y-2">
+          <Label htmlFor="name">Name</Label>
+          <Input id="name" defaultValue="John Doe" />
+        </div>
+        {/* Email */}
+        <div className="space-y-2">
+          <Label htmlFor="email">Email</Label>
+          <Input id="email" defaultValue="name@example.com" />
+        </div>
+      </Tabs>
+
+      <Tabs className="space-y-4">
+        <Button className="w-full sm:w-auto mt-[300px]" variant={'outline'}>
+          Change password
+        </Button>
+        <Button className="w-full sm:w-auto">Save changes</Button>
       </Tabs>
     </div>
   );
