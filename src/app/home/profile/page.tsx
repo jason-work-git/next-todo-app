f9733dd22ea5a9f393c23140b04eee591833e07e
Daniel <jamoja64749@gmail.com>
2024-11-15 12:28:59 +0100

    Removed unused components

--- src/app/home/profile/page.tsx
+++ src/app/home/profile/page.tsx
@@ -1,37 +1,35 @@
+'use client';
+
 import { Button } from '@/components/ui/button';
 import { Label } from '@/components/ui/label';
 import { Input } from '@/components/ui/input';
-import { Tabs } from '@/components/ui/tabs';
 
 export default function Page() {
   return (
-    <div className="space-y-6 p-4 sm:p-8">
+    <div className="space-y-6">
       <div className="text-center">
-        <h2 className="text-6xl font-bold tracking-tight">Settings</h2>
+        <h2 className="text-3xl font-bold tracking-tight">Settings</h2>
         <p className="text-muted-foreground">
           Manage your account settings and set e-mail preferences.
         </p>
       </div>
-      <Tabs className="space-y-4">
-        {/* Profile Name */}
-        <div className="space-y-2 max-w-md w-full">
-          <Label htmlFor="name">Name</Label>
-          <Input id="name" defaultValue="John Doe" />
-        </div>
-        {/* Email */}
-        <div className="space-y-2 max-w-md w-full">
-          <Label htmlFor="email">Email</Label>
-          <Input id="email" defaultValue="name@example.com" disabled />
-        </div>
-      </Tabs>
-      <Tabs className="space-y-4">
-        <Button className="w-full sm:w-auto" variant="outline">
-          Change password
-        </Button>
-        <Button className="w-full sm:w-auto ml-1" variant="default">
-          Save changes
-        </Button>
-      </Tabs>
+      {/* Profile Name */}
+      <div className="space-y-2 max-w-md w-full">
+        <Label htmlFor="name">Name</Label>
+        <Input id="name" defaultValue="John Doe" />
+      </div>
+      {/* Email */}
+      <div className="space-y-2 max-w-md w-full">
+        <Label htmlFor="email">Email</Label>
+        <Input id="email" defaultValue="name@example.com" disabled />
+      </div>
+      {/* Change Password */}
+      <Button className="w-full sm:w-auto" variant="outline">
+        Change password
+      </Button>
+      <Button className="w-full sm:w-auto ml-1" variant="default">
+        Save changes
+      </Button>
     </div>
   );
 }
