03b420ab6999788406301c8b7dfaaec68d7e3d1d
Junk-debug <maks.naumovich@gmail.com>
2024-11-16 15:34:11 +0100

    feat: add useDeleteTaskMutation hook to handle task deletion, add delete task button

---
+++ src/components/delete-task-button.tsx
@@ -0,0 +1,69 @@
+import { Button, ButtonProps } from './ui/button';
+import {
+  Drawer,
+  DrawerTrigger,
+  DrawerContent,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerClose,
+} from './ui/drawer';
+
+import { Task } from '@prisma/client';
+
+import useDeleteTaskMutation from '@/hooks/useDeleteTaskMutation';
+import { useState } from 'react';
+
+export const DeleteTaskButton = ({
+  taskId,
+  onDelete,
+  ...props
+}: ButtonProps & { taskId: Task['id']; onDelete?: () => void }) => {
+  const [open, setOpen] = useState(false);
+  const { mutate } = useDeleteTaskMutation({
+    onMutate: () => {
+      setOpen(false);
+    },
+    onSettled: () => {
+      if (onDelete) {
+        onDelete();
+      }
+    },
+  });
+
+  return (
+    <Drawer open={open} onOpenChange={setOpen} nested>
+      <DrawerTrigger asChild>
+        <Button
+          className="hover:bg-destructive hover:text-destructive-foreground"
+          variant={'outline'}
+          {...props}
+        >
+          Delete
+        </Button>
+      </DrawerTrigger>
+
+      <DrawerContent>
+        <DrawerHeader>
+          <DrawerTitle>Are you sure?</DrawerTitle>
+          <DrawerDescription>
+            This action cannot be undone. This will permanently delete your
+            task.
+          </DrawerDescription>
+        </DrawerHeader>
+        <DrawerFooter>
+          <DrawerClose asChild>
+            <Button variant={'outline'}>Cancel</Button>
+          </DrawerClose>
+          <Button
+            onClick={() => mutate({ id: taskId })}
+            variant={'destructive'}
+          >
+            Delete
+          </Button>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
+  );
+};
---
+++ src/hooks/useDeleteTaskMutation.ts
@@ -0,0 +1,92 @@
+import { DeleteTaskDto } from '@/lib/actions/task/types';
+import { Task } from '@prisma/client';
+
+import { deleteTask } from '@/lib/actions/task/controller';
+import {
+  useQueryClient,
+  useMutation,
+  UseMutationOptions,
+} from '@tanstack/react-query';
+
+import { toast } from 'sonner';
+
+type TContext = {
+  previousTasks: Task[] | undefined;
+  previousTask: Task | undefined;
+};
+
+type UseDeleteTaskMutationOptions = Omit<
+  UseMutationOptions<Task, Error, DeleteTaskDto, TContext>,
+  'mutationFn' | 'onMutate'
+> & {
+  onMutate?: (data: DeleteTaskDto) => void;
+};
+
+export default function useDeleteTaskMutation({
+  onMutate,
+  onError,
+  onSuccess,
+  ...options
+}: UseDeleteTaskMutationOptions = {}) {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation<Task, Error, DeleteTaskDto, TContext>({
+    mutationFn: deleteTask,
+    onMutate: (data) => {
+      const { id } = data;
+      const previousTasks = queryClient.getQueryData<Task[]>(['tasks']);
+      const previousTask = queryClient.getQueryData<Task>(['tasks', id]);
+
+      queryClient.setQueryData<Task>(['tasks', id], undefined);
+      queryClient.setQueryData<Task[]>(['tasks'], (oldTasks) => {
+        if (!oldTasks) {
+          return oldTasks;
+        }
+
+        return oldTasks.filter((task) => task.id !== id);
+      });
+
+      if (onMutate) {
+        onMutate(data);
+      }
+
+      return { previousTasks, previousTask };
+    },
+    onError: (error, variables, context) => {
+      if (context?.previousTasks) {
+        queryClient.setQueryData(['tasks'], context.previousTasks);
+      }
+
+      if (context?.previousTask) {
+        queryClient.setQueryData(
+          ['tasks', context.previousTask.id],
+          context.previousTask,
+        );
+      }
+
+      toast.error('Failed to delete task', {
+        richColors: true,
+        duration: 5000,
+        closeButton: true,
+      });
+
+      if (onError) {
+        onError(error, variables, context);
+      }
+    },
+    onSuccess: (deletedTask, variables, context) => {
+      toast.success('Task deleted successfully', {
+        richColors: true,
+        duration: 5000,
+        closeButton: true,
+      });
+
+      if (onSuccess) {
+        onSuccess(deletedTask, variables, context);
+      }
+    },
+    ...options,
+  });
+
+  return mutation;
+}
