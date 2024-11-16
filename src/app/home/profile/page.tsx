39be7aed00b8929f35d8aab7f78c4416f4487f0a
Junk-debug <maks.naumovich@gmail.com>
2024-11-16 19:51:27 +0100

    fix: fix error with server actions

--- src/app/home/profile/page.tsx
+++ src/app/home/profile/page.tsx
@@ -6,24 +6,26 @@ export default async function ProfilePage() {
 
   return (
     <main className="p-8 flex items-center justify-center min-h-dvh">
-      <div className="min-w-64 w-72 flex flex-col space-y-4">
+      <div className="min-w-64 w-72 flex flex-col items-center space-y-4">
         <h1 className="text-center tracking-tight text-3xl font-semibold">
           Profile
         </h1>
         <span className="text-xs self-center text-muted-foreground">
-          {session?.user?.email}
+          Email: {session?.user?.email}
         </span>
         <span className="text-xs self-center text-muted-foreground">
-          {session?.user?.name}
+          Name: {session?.user?.name}
         </span>
+        <form
+          action={async () => {
+            'use server';
+
+            await signOut();
+          }}
+        >
+          <Button>Logout</Button>
+        </form>
       </div>
-      <form
-        action={async () => {
-          await signOut();
-        }}
-      >
-        <Button>Logout</Button>
-      </form>
     </main>
   );
 }
