f549d93c37c9b1c887e020fe7fbc88ef05b8f0a9
Junk-debug <maks.naumovich@gmail.com>
2024-11-19 21:28:24 +0100

    feat: change task-details ui, use edit task form to edit task, add date-select to form, delete edit-task-button

--- src/components/edit-task-form.tsx
+++ src/components/edit-task-form.tsx
@@ -1,29 +1,34 @@
 'use client';
 
-import { DrawerFooter } from '@/components/ui/drawer';
 import { Button } from '@/components/ui/button';
 
 import { Label } from './ui/label';
 import { Input } from './ui/input';
 import { Textarea } from './ui/textarea';
 import { useState } from 'react';
+import { DateSelect } from './date-select';
+import { cn } from '@/lib/utils';
 
 export type EditFormData = {
   title: string;
   description: string | null;
+  dueDate: Date | null;
 };
 
 export const EditTaskForm = ({
   onSubmit,
   initialState,
-}: {
+  className,
+  ...props
+}: Omit<React.HTMLAttributes<HTMLFormElement>, 'onSubmit'> & {
   initialState: EditFormData;
   onSubmit: (formData: EditFormData) => void;
 }) => {
   const [formData, setFormData] = useState<EditFormData>(initialState);
   const isChanged =
     formData.title !== initialState.title ||
-    formData.description !== initialState.description;
+    formData.description !== initialState.description ||
+    formData.dueDate?.getDate() !== initialState.dueDate?.getDate();
 
   const onChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
@@ -37,7 +42,11 @@ export const EditTaskForm = ({
   };
 
   return (
-    <form onSubmit={handleSubmit} className="px-4 space-y-2">
+    <form
+      onSubmit={handleSubmit}
+      className={cn('px-4 flex flex-col gap-4', className)}
+      {...props}
+    >
       <Label className="flex flex-col gap-2">
         Title
         <Input
@@ -55,11 +64,18 @@ export const EditTaskForm = ({
           onChange={onChange}
         />
       </Label>
-      <DrawerFooter className="px-0">
-        <Button disabled={!isChanged} type="submit">
-          Save
-        </Button>
-      </DrawerFooter>
+      <div className="flex flex-col gap-2 items-start">
+        <span className="text-sm font-medium leading-none">Due date</span>
+        <DateSelect
+          // disabled={isPending}
+          selectedDate={formData.dueDate}
+          onSelectDate={(value) => setFormData({ ...formData, dueDate: value })}
+        />
+      </div>
+
+      <Button disabled={!isChanged} type="submit">
+        Save
+      </Button>
     </form>
   );
 };
--- src/components/task-details.tsx
+++ src/components/task-details.tsx
@@ -1,63 +1,51 @@
 'use client';
 
-import { Text } from 'lucide-react';
-
 import {
   DrawerDescription,
   DrawerFooter,
   DrawerHeader,
   DrawerTitle,
 } from '@/components/ui/drawer';
-import { Checkbox } from '@/components/ui/checkbox';
-import { DateSelect } from '@/components/date-select';
+
 import { DeleteTaskButton } from './delete-task-button';
-import { EditTaskButton } from './edit-task-button';
 
 import { Task } from '@prisma/client';
 import { useTaskDrawerData } from './task-drawer-provider';
 import useUpdateTaskMutation from '@/hooks/useUpdateTaskMutation';
+import { EditFormData, EditTaskForm } from './edit-task-form';
 
 export const TaskDetails = ({ task }: { task: Task }) => {
-  const { mutate } = useUpdateTaskMutation();
   const { close } = useTaskDrawerData();
+  const { mutate } = useUpdateTaskMutation({
+    onMutate: () => close(),
+  });
+
+  const handleSubmit = (formData: EditFormData) => {
+    mutate({
+      id: task.id,
+      title: formData.title,
+      description: formData.description,
+      dueDate: formData.dueDate,
+    });
+  };
 
   return (
     <>
       <DrawerHeader>
-        <div className="flex gap-2 items-center">
-          <Checkbox
-            checked={task.completed}
-            onCheckedChange={(value: boolean) => {
-              mutate({ id: task.id, completed: value });
-            }}
-            className="size-5 rounded-full"
-          />
-          <DrawerTitle className="text-start peer-data-[state=checked]:line-through peer-data-[state=checked]:text-muted-foreground">
-            {task.title}
-          </DrawerTitle>
-        </div>
-        {task.description ? (
-          <div className="flex gap-2">
-            <Text className="size-5 text-muted-foreground" />
-            <DrawerDescription className="text-start flex-1">
-              {task.description}
-            </DrawerDescription>
-          </div>
-        ) : (
-          <DrawerDescription hidden>No description</DrawerDescription>
-        )}
+        <DrawerTitle>Edit task</DrawerTitle>
+        <DrawerDescription>Provide new task details</DrawerDescription>
       </DrawerHeader>
-      <div className="px-4">
-        <DateSelect
-          onSelectDate={(dueDate) => {
-            mutate({ id: task.id, dueDate: dueDate });
-          }}
-          selectedDate={task.dueDate}
-        />
-      </div>
+      <EditTaskForm
+        className="pb-0"
+        initialState={{
+          title: task.title,
+          description: task.description,
+          dueDate: task.dueDate,
+        }}
+        onSubmit={handleSubmit}
+      />
 
       <DrawerFooter>
-        <EditTaskButton task={task} />
         <DeleteTaskButton taskId={task.id} onDelete={close} />
       </DrawerFooter>
     </>
