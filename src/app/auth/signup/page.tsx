4de5520fc92bfaecc844b952438fb2e09a47a7f1
Junk-debug <maks.naumovich@gmail.com>
2024-11-21 23:33:39 +0100

    refactor: add auth layout, move forgot password modal to separate component and make it adaptive using useMediaQuery hook, add useMediaQuery hook to access media queries

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -4,25 +4,23 @@ import AuthProviders from '@/components/auth-providers';
 
 export default function SignUpPage() {
   return (
-    <main className="p-8 flex items-center justify-center min-h-dvh">
-      <div className="min-w-64 w-72 flex flex-col space-y-4">
-        <h1 className="text-center tracking-tight text-3xl mb-6 font-semibold">
-          Create an account
-        </h1>
-        <SignUpForm />
+    <div className="min-w-64 w-72 flex flex-col space-y-4">
+      <h1 className="text-center tracking-tight text-3xl mb-6 font-semibold">
+        Create an account
+      </h1>
+      <SignUpForm />
 
-        <AuthProviders />
+      <AuthProviders />
 
-        <span className="text-xs self-center text-muted-foreground">
-          Already have an account?{' '}
-          <Link
-            href={'/auth/login/'}
-            className="text-primary hover:underline cursor-pointer"
-          >
-            Sign in
-          </Link>
-        </span>
-      </div>
-    </main>
+      <span className="text-xs self-center text-muted-foreground">
+        Already have an account?{' '}
+        <Link
+          href={'/auth/login/'}
+          className="text-primary hover:underline cursor-pointer"
+        >
+          Sign in
+        </Link>
+      </span>
+    </div>
   );
 }
