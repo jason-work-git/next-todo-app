b6be4790c01d4445c00579b5e5baee30de86c471
Junk-debug <maks.naumovich@gmail.com>
2024-11-23 00:23:36 +0100

    fix: disable bottom padding on medium screen

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -13,7 +13,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <Tabs asChild value={pathname} className="h-full">
       <div className="!h-dvh flex flex-col">
-        <main className="flex flex-col flex-grow overflow-y-auto px-8 pt-4 pb-[4.25rem]">
+        <main className="flex flex-col flex-grow overflow-y-auto px-8 pt-4 pb-[4.25rem] md:pb-0">
           <TaskDrawerProvider>{children}</TaskDrawerProvider>
         </main>
         <TabsList className="flex w-full justify-evenly fixed bottom-0 md:hidden py-2 h-[4.25rem] rounded-none">
