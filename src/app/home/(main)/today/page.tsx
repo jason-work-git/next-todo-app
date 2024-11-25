470f656fc8d05861bc5265b21c814174292979c5
Daniel <jamoja64749@gmail.com>
2024-11-25 19:12:33 +0100

    Fixed bugs with the skeleton loader not popping up

--- src/app/home/(main)/today/page.tsx
+++ src/app/home/(main)/today/page.tsx
@@ -1,52 +1,49 @@
 'use client';
-
 import { AddTaskButton } from '@/components/add-task-button';
 import { TaskCard } from '@/components/task-card';
-
 import { getTasks } from '@/actions/task/controller';
 import { useQuery } from '@tanstack/react-query';
 import { filterTodayTasks, getFormattedDate } from '@/lib/utils';
 import { Skeleton } from '@/components/ui/skeleton';
-
 export default function MainPage() {
   const formattedToday = getFormattedDate(new Date());
-
   const { data, isLoading } = useQuery({
     queryFn: getTasks,
     queryKey: ['tasks'],
   });
-
   const tasks = data ? filterTodayTasks(data) : [];
-
   return (
     <>
       <header className="mb-4">
         <h1 className="text-xl font-semibold text-center">Today</h1>
       </header>
-
       <h2 className="font-medium text-muted-foreground mb-2">
         {formattedToday}
       </h2>
 
-      {isLoading ? (
+      {isLoading && (
         <>
-          <Skeleton className="w-full h-[40px] mb-5" />
+          <Skeleton className="w-full h-[2.5rem] mb-5" />
         </>
-      ) : tasks.length === 0 ? (
-        <div className="text-center font-medium flex justify-center items-center h-full">
-          Nothing on your plate today, enjoy your free time! 🎉
-        </div>
-      ) : (
-        <div className="flex-grow overflow-y-auto space-y-2 p-px">
-          {tasks.map((task) => (
-            <TaskCard showDueDate={false} key={task.id} task={task} />
-          ))}
-          <AddTaskButton
-            className="fixed bottom-20 right-8"
-            defaultDueDate={new Date()}
-          />
-        </div>
       )}
+
+      {isLoading && tasks.length === 0 && (
+        <>
+          <div className="text-center font-medium flex justify-center items-center h-full">
+            Nothing on your plate today, enjoy your free time! 🎉
+          </div>
+        </>
+      )}
+
+      <div className="flex-grow overflow-y-auto space-y-2 p-px">
+        {tasks.map((task) => (
+          <TaskCard showDueDate={false} key={task.id} task={task} />
+        ))}
+        <AddTaskButton
+          className="fixed bottom-20 right-8"
+          defaultDueDate={new Date()}
+        />
+      </div>
     </>
   );
 }
