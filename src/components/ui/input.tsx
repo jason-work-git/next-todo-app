7456ab229813673e95acdbc581c3b7608cd142c7
Junk-debug <maks.naumovich@gmail.com>
2024-11-26 10:08:53 +0100

    feat: add default placeholder when input type is email

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -6,7 +6,7 @@ export type InputProps = React.InputHTMLAttributes<HTMLInputElement> & {
   ref?: React.Ref<HTMLInputElement>;
 };
 
-const Input = ({ className, type, ref, ...props }: InputProps) => {
+const Input = ({ className, type, placeholder, ref, ...props }: InputProps) => {
   return (
     <input
       type={type}
@@ -14,6 +14,9 @@ const Input = ({ className, type, ref, ...props }: InputProps) => {
         'flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
         className,
       )}
+      placeholder={
+        placeholder || (type === 'email' ? 'name@example.com' : undefined)
+      }
       ref={ref}
       {...props}
     />
