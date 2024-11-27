850d73e8a949df55250c7f37bef40f722b4aa8bb
OleksandraNochovna <nochovna.sasha@gmail.com>
2024-11-27 20:43:33 +0100

    implement updateUser action

--- src/actions/user/controller.ts
+++ src/actions/user/controller.ts
@@ -3,9 +3,15 @@
 import { UpdateUserDto } from './types';
 import { userService } from './service';
 import { requireAuth } from '../auth/middlewares';
+import { revalidatePath } from 'next/cache';
 
 export const updateUser = requireAuth(
-  async ({ session }, data: UpdateUserDto) => {
-    return userService.updateUser(session.user.id, data);
+  async ({ session }, data: Pick<UpdateUserDto, 'name' | 'password'>) => {
+    const user = await userService.updateUser(session.user.id, data);
+    const normalizedUser = userService.normalizeUser(user);
+
+    revalidatePath('/home/profile');
+
+    return normalizedUser;
   },
 );
